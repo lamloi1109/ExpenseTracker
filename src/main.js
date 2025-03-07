2bf80bfcdc0cae4c290f3e1d568ed9ac150c92d9
lamloi1109 <lamloi12a1@gmail.com>
2025-03-07 20:23:15 +0700

    chore: Init project

---
+++ src/main.js
@@ -0,0 +1,57 @@
+// startup dự án
+// chia branch ĐEV
+
+// - Thêm chi phí với mô tả hoạt động và số tiền đi kèm
+// commit -m 'feat: Add expense'
+// + Xử lý đầu vào
+// + Xử lý commander
+// + Xử lý đọc ghi file
+// + Ghi file
+// + Đọc file
+// + Chuyển tử chuổi sang json
+// + Chuyển từ json sang chuỗi
+// + Tạo instance của expense 
+// - Id
+// - description
+// - created_at
+// - updated_at
+// - amount
+// - caterogy
+// - max
+// - min
+
+// - Cập nhật chi phí
+// commit -m 'feat: Update expense'
+// Nhân tham số đầu vào
+// Xử lý action từ command
+// Load dữ liệu từ database
+// Tìm expense có id là.... 
+// Cập nhật giá trị của id đó
+// Ghi lại thay đổi vào file json
+
+// - Xóa chi phí
+// commit -m 'feat: Delete expense'
+// Tìm id 
+
+// - Xem danh sách tất cả chi phí
+// commit -m 'feat: View all expense'
+
+// - Xem tổng của tất cả chi phí
+// commit -m 'feat: Sum all expense'
+
+// - Xem tổng của chi phí chỉ đinh theo tháng
+// commit -m 'feat: View expense by month'
+
+// - Phân danh mục cho các loại chi phí
+// commit -m 'feat: Filter by category'
+
+// - Cho phép người dùng thiết lập giới hạn chi tiêu
+// commit -m 'feat: setMaxExpense'
+
+// + Nếu vượt quá giới hạn thì cảnh báo
+// commit -m 'feat: Check max'
+
+// + Cho phép người dùng xuất ra định dạng CSV, XLS, ...
+// commit -m 'feat: exportData'
+
+// - Chức năng help để giới thiệu về các chức năng trong dự án
